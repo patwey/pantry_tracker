1f08b6664dde6c4c00656e24fd594ed17c0df128
Pat Wey <patrickwey@gmail.com>
2016-04-06 15:03:25 -0700

    Update error message on email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true,
                     uniqueness: true,
-                    format: { with: /\A[\w]+@[\w]+.[\w]+\z/ }
+                    format: { with: /\A[\w]+@[\w]+.[\w]+\z/,
+                              message: 'improper email format (e.g. johndoe@example.com)' }
 end
