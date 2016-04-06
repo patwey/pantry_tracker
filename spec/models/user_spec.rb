629803f620dc25605627196ef340358c7eb240a1
Pat Wey <patrickwey@gmail.com>
2016-04-06 14:30:40 -0700

    Create user model
    - validate name, email, password
    - secure password using bcrypt

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  def valid_user
+    User.new(name: 'Archie',
+             email: 'archie@example.com',
+             password: 'password')
+  end
+
+  it 'is valid' do
+    expect(valid_user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = valid_user
+    user.name = nil
+
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = valid_user
+    user.email = nil
+
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid without a unique email' do
+    valid_user.save
+    user = valid_user
+
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid without a properly formatted email' do
+    user = valid_user
+    user.email = 'www.google.com'
+
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid without a password' do
+    user = valid_user
+    user.password = nil
+
+    expect(user).not_to be_valid
+  end
+end
