5efb86df501105ff0642dc8860d73a92f08a5bc0
Pat Wey <patrickwey@gmail.com>
2016-04-08 20:20:34 -0700

    Create seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true,
                     uniqueness: true,
-                    format: { with: /\A[\w]+@[\w]+.[\w]+\z/,
+                    format: { with: /\A[\w._]+@[\w]+.[\w]+\z/,
                               message: 'improper email format (e.g. johndoe@example.com)' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def self.start
+    create_users
+    create_ingredients
+    create_user_ingredients
+  end
+
+  def self.create_users
+    20.times do
+      User.create!(name: Faker::Name.name,
+                   email: Faker::Internet.email,
+                   password: 'password')
+    end
+  end
+
+  def self.create_ingredients
+    fruits = ['Red Apples', 'Blood Oranges', 'Cherries', 'Cranberries', 'Red Grapes', 'Red Grapefruit', 'Pears', 'Pomegranates', 'Raspberries', 'Strawberries', 'Watermelon',	'Beets', 'Red Peppers', 'Radishes', 'Radicchio', 'Red Onions', 'Red Potatoes', 'Rhubarb', 'Tomatoes']
+    units = %w(ounces grams)
+    fruits.each do |fruit|
+      Ingredient.create(name: fruit,
+                        units: units.sample)
+    end
+  end
+
+  def self.create_user_ingredients
+    u_ids = User.pluck(:id)
+    i_ids = Ingredient.pluck(:id)
+    50.times do
+      UserIngredient.create!(user_id:       u_ids.sample,
+                             ingredient_id: i_ids.sample,
+                             amount:        Random.rand(10))
+    end
+  end
+end
+
+Seed.start
